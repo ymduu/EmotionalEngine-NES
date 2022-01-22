fb2e646a561f190163a258ba1396fd3dd68541d0
ymduu <ymd2007umd@gmail.com>
2022-01-23 04:28:18 +0900

    (EENES-26) 矩形波チャンネルクラスとレジスタの読み書きを実装

--- Programs/src/Apu.cpp
+++ Programs/src/Apu.cpp
@@ -3,6 +3,84 @@
 #include <utility>
 #include "Apu.h"
 
+namespace {
+	int g_DutyTables[4][8] = {
+		{0, 1, 0, 0, 0, 0, 0, 0},
+		{0, 1, 1, 0, 0, 0, 0, 0},
+		{0, 1, 1, 1, 1, 0, 0, 0},
+		{1, 0, 0, 1, 1, 1, 1, 1},
+	};
+	constexpr int ClocksToNextSequence = 7457;
+}
+
 namespace nes { namespace detail {
+	void SquareWaveChannel::WriteRegister(uint8_t value, uint16_t addr)
+	{
+		uint16_t offset = addr - m_BaseAddr;
+
+		switch (offset) {
+		case 0 :
+			m_DutyTable = g_DutyTables[value >> 6];
+			m_DecayLoop = (value >> 5) & 1;
+			m_LengthEnabled = !m_DecayLoop;
+			m_DecayEnabled = ((value >> 4) & 1) == 0;
+			m_DecayV = value & 0b1111;
+			break;
+		case 1:
+			m_SweepTimer = (value >> 4) & 0b111;
+			m_SweepNegate = (value >> 3) & 1;
+			m_SweepShift = value & 0b111;
+			m_SweepReload = true;
+			m_SweepEnabled = ((value >> 7) & 1) == 1 && m_SweepShift != 0;
+			break;
+		case 2:
+			// m_FreqTImer の上位3bitだけ残してクリア
+			m_FreqTimer &= 0b11100000000;
+			// m_FreqTimer の 下位8bitを更新
+			m_FreqTimer |= value;
+			break;
+		case 3:
+		{
+			// m_FreqTimer の上位3 bit をクリア
+			m_FreqTimer &= 0b11111111;
+			uint16_t hi = value & 0b111;
+			m_FreqTimer |= (hi << 8);
+
+			// TODO: length Counter 更新
+			// m_LengthCounter = lengthtable[]
+
+			m_FreqCounter = m_FreqTimer;
+			m_DutyCounter = 0;
+			m_DecayResetFlag = true;
+			break;
+		}
+		default:
+			break;
+		}
+	}
+
+	void SquareWaveChannel::On4015Write(bool channelEnabled)
+	{
+		m_ChannelEnabled = channelEnabled;
+		if (!m_ChannelEnabled) {
+			m_LengthCounter = 0;
+		}
+	}
+
+	// 割り込みに関する処理は APU 全体のほうでやることに注意
+	void SquareWaveChannel::On4017Write(SequencerMode mode)
+	{
+		m_SequencerMode = mode;
+		m_NextSeqPhase = 0;
+		// フレームシーケンサのクロックは CPU クロックで計算する。(half clock を考慮するのをサボるため)
+		// Every APU Cycle の実装では毎回2引くことに注意する。
+		m_SequencerCounter = ClocksToNextSequence;
+
+		if(m_SequencerMode == Mode_5Step)
+		{
+			// TODO: QuaterFrame と HalfFrame 相当の処理よびだし 
+		}
+	}
+
 
 }}
--- Programs/src/Apu.h
+++ Programs/src/Apu.h
@@ -6,5 +6,91 @@
 
 namespace nes { namespace detail {
 	class CpuBus;
+	enum SequencerMode
+	{
+		Mode_4Step,
+		Mode_5Step
+	};
+
+	// 矩形波 チャンネル、コメントのアドレスは矩形波チャンネル1のものであるとする
+	class SquareWaveChannel {
+	public:
+		SquareWaveChannel(uint16_t baseAddr)
+			: m_DutyTable(nullptr) // TORIAEZU: 設定する前に音ならす ROM があってぬるぽ触ってしまうようなら考える
+			, m_DecayLoop(false)
+			, m_LengthEnabled(false)
+			, m_DecayEnabled(false)
+			, m_DecayV(0)
+			, m_SweepTimer(0)
+			, m_SweepNegate(false)
+			, m_SweepShift(0)
+			, m_SweepReload(0)
+			, m_SweepEnabled(0)
+			, m_FreqTimer(0)
+			, m_LengthCounter(0)
+			, m_ChannelEnabled(false)
+			, m_SequencerMode(Mode_4Step)
+			, m_IrqEnabled(false)
+			, m_FreqCounter(0)
+			, m_DutyCounter(0)
+			, m_DecayResetFlag(false)
+			, m_NextSeqPhase(0)
+			, m_SequencerCounter(0)
+			, m_CpuClock(0)
+			, m_ApuClock(0)
+			, m_BaseAddr(baseAddr)
+		{}
+
+		void WriteRegister(uint8_t value, uint16_t addr);
+		// APU 全体レジスタ($4015, $4017 の書き込みで反映される値)
+		void On4015Write(bool channelEnabled);
+		void On4017Write(SequencerMode mode);
+
+	private:
+		// エミュレーション用変数たち、読み取り専用の値はレジスタからフェッチするだけでいいはずだが、どれが読み取り専用かわからんので全部変数にしちゃう、レジスタ書き込み時に設定することにする
+		
+		// $4000
+		// Duty Table 配列先頭を指すポインタ
+		int* m_DutyTable;
+		bool m_DecayLoop;
+		bool m_LengthEnabled;
+		bool m_DecayEnabled;
+		int m_DecayV;
+	
+		// $4001
+		int m_SweepTimer;
+		bool m_SweepNegate;
+		int m_SweepShift;
+		bool m_SweepReload;
+		bool m_SweepEnabled;
+
+		// $4002, $4003
+		int m_FreqTimer;
+		int m_LengthCounter;
+
+		// $4015、チャンネルによってことなる bit を見ることに注意
+		bool m_ChannelEnabled;
+
+		// $4017
+		SequencerMode m_SequencerMode;
+		bool m_IrqEnabled;
+
+		// レジスタの値ではないけどエミュレーションにひつよう
+		// $4003 書き込みで初期化されるひとたち
+		int m_FreqCounter;
+		int m_DutyCounter;
+		bool m_DecayResetFlag;
+
+		// $4017 書き込みで初期化されるひとたち
+		int m_NextSeqPhase;
+		int m_SequencerCounter;
+
+		// クロックたち、 CPU クロックと APU クロックの両方をもつ(CPU クロックで動く人もいるので)
+		uint64_t m_CpuClock;
+		uint64_t m_ApuClock;
+
+		// 矩形波チャンネルレジスタのベースアドレス
+		uint16_t m_BaseAddr;
+	};
 
 }}

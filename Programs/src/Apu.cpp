21610e35154341b09204340bd99a6564d19eaf21
ymduu <ymd2007umd@gmail.com>
2022-01-23 02:56:01 +0900

    (EENES-26) APU を実装するファイルを追加

---
+++ Programs/src/Apu.cpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <cassert>
+#include <utility>
+#include "Apu.h"
+
+namespace nes { namespace detail {
+
+}}
---
+++ Programs/src/Apu.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <utility>
+#include <stdint.h>
+#include "constants.h"
+#include "System.h"
+
+namespace nes { namespace detail {
+	class CpuBus;
+
+}}
